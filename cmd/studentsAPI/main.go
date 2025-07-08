fc77ea0a68f924ad3e8e7c46b67295ff5d0d73b1
pixel2packet <pritesh@intelxlabs.com>
2025-07-08 14:07:32 +0530

    add gracefully shutdown

--- cmd/studentsAPI/main.go
+++ cmd/studentsAPI/main.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/pixel2packet/studentAPI/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to StudentsAPI")
+	// load config
+	cfg := config.MustLoad()
+
+	// database setup
+
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+	})
+
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("server started", slog.String("addr", cfg.Addr))
+
+	// fmt.Printf("server started on: %s", cfg.HTTPServer.Addr)
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGALRM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	// or
+
+	// if err != nil {
+	// 	slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	// }
+
+	// server.Shutdown()
+
+	slog.Info("server shutdown successfully")
+
 }
